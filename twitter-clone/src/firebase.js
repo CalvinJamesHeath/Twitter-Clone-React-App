82e0c991a1a31a0ef048b7e2c844958e332cc74e
Calvin James Heath <72168052+CalvinJamesHeath@users.noreply.github.com>
2021-03-02 16:27:55 -0300

    Update firebase.js

--- twitter-clone/src/firebase.js
+++ twitter-clone/src/firebase.js
@@ -2,14 +2,14 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB_AHMCn3vRddqTiEOfLyOd5uLPhCtCa2E",
+    apiKey: "",
     authDomain: "twitter-clone-fda1f.firebaseapp.com",
     databaseURL: "https://twitter-clone-fda1f.firebaseio.com",
     projectId: "twitter-clone-fda1f",
     storageBucket: "twitter-clone-fda1f.appspot.com",
     messagingSenderId: "424865126840",
-    appId: "1:424865126840:web:15d3f4f80850bbc208d74d",
-    measurementId: "G-6ZVWJ3EWDK"
+    appId: "",
+    measurementId: ""
   };
   
 const firebaseApp = firebase.initializeApp(firebaseConfig);
